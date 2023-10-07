dc062443e42d556c15f5f0349303433880fee044
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-10-07 17:28:56 -0400

    Basic Opencv Setup

---
+++ stream_to_index.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import os
+
+
+# make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
+
+# instatiate flask app
+app = Flask(__name__, template_folder='./templates')
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        success, frame = camera.read()
+
+        if success:
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run()
