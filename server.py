6b018aba225607eb3a6199c3a63136592c9d32e4
Nick T <nicholastroiano123@gmail.com>
2023-10-10 09:43:58 -0400

    math class wild

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 import threading
 import cv2
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt install python3 -y
+python get-pip.py
+pip install Flask
+pip install Flask-SocketIO
+pip install opencv-python
