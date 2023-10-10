c51729eb3dbaa0ecb303c9b0a28b363f49c35ef5
Nick T <nicholastroiano123@gmail.com>
2023-10-10 09:58:31 -0400

    nevermind

--- setup.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install python3 -y
-python get-pip.py
-pip install Flask
-pip install Flask-SocketIO
-pip install opencv-python
-sudo snap install ngrok
-ngrok config add-authtoken xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
